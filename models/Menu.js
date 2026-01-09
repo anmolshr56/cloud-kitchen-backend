d2e6bb5d31df718ca7b5060ce5e83a258378119e
Anmol Sharma <anmolsharma.4806@outlook.com>
2026-01-09 21:20:48 +0530

    Create Menu model with schema for MongoDB

---
+++ models/Menu.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const menuSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    isAvailable: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Menu", menuSchema);
