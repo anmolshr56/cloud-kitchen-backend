813a2e8f1765b65b9229a28a977ea9bfdb9449a5
Anmol Sharma <anmolsharma.4806@outlook.com>
2026-01-10 00:27:53 +0530

    Refactor User schema to include lowercase email

--- models/User.js
+++ models/User.js
@@ -2,15 +2,21 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
     role: {
       type: String,
       enum: ["SUPER_ADMIN", "ADMIN"],
-      default: "ADMIN",
-      immutable: true,
-    },
+      default: "ADMIN"
+    }
   },
   { timestamps: true }
 );
