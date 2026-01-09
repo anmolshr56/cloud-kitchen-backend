61de8051891c680dc83e8d432663393c73ab6355
Anmol Sharma <anmolsharma.4806@outlook.com>
2026-01-10 00:30:17 +0530

    Simplify error handling for MongoDB connection

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error(err));
+  .catch(console.error);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
