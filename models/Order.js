8b49f477444a6804614f0bc3b1c562f6b93b5b10
Anmol Sharma <anmolsharma.4806@outlook.com>
2026-01-10 20:50:23 +0530

    Add customerAddress to order schema
    Added customerAddress field to order schema.

--- models/Order.js
+++ models/Order.js
@@ -4,39 +4,37 @@ const orderSchema = new mongoose.Schema(
   {
     customerName: {
       type: String,
-      required: true
+      required: true,
     },
     customerPhone: {
       type: String,
-      required: true
+      required: true,
+    },
+    customerAddress: {
+      type: String,
+      required: true, // 👈 NEW
     },
     items: [
       {
         menuId: {
           type: mongoose.Schema.Types.ObjectId,
           ref: "Menu",
-          required: true
+          required: true,
         },
         name: String,
         price: Number,
-        quantity: Number
-      }
+        quantity: Number,
+      },
     ],
     totalAmount: {
       type: Number,
-      required: true
+      required: true,
     },
     status: {
       type: String,
-      enum: [
-        "PENDING",
-        "ACCEPTED",
-        "PREPARING",
-        "DELIVERED",
-        "CANCELLED"
-      ],
-      default: "PENDING"
-    }
+      enum: ["PENDING", "PREPARING", "DELIVERED"],
+      default: "PENDING",
+    },
   },
   { timestamps: true }
 );
